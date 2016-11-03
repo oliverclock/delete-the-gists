c74933779addc56c276ea5c5e802f8dab50b5b3b
Oliver Lock <oliverclock@gmail.com>
2016-11-03 22:06:44 +1100

    Update gist_delete.py

--- gist_delete.py
+++ gist_delete.py
@@ -1,8 +1,8 @@
 #install simplegist, i.e. pip install simplegist
 from simplegist import Simplegist
 #get a personal token for gist from github.com, use your username and token here
-GHgist = Simplegist(username='oliverclock',api_token='58ef0133e2096c774e8be7f4e1f836f8f68a7508')
-list = GHgist.search('oliverclock').listall()
+GHgist = Simplegist(username='your_username',api_token='your_token')
+list = GHgist.search('your_usernamek').listall()
 for line in list: 
     print line
     GHgist.profile().delete(name=line)
